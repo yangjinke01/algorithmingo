e4dae01fdcd963a2ec4b09b79ca037806d9b640d
jackplus <yangjinke01@qq.com>
2022-01-16 18:57:49 +0800

    interfase

---
+++ basic/018interface/interface.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perimeter() float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+
+func (r rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (r rectangle) perimeter() float64 {
+	return r.width*2 + r.height*2
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println("area:", g.area())
+	fmt.Println("perimeter", g.perimeter())
+}
+
+func main() {
+	measure(rectangle{width: 5, height: 5})
+	measure(circle{radius: 3})
+}
