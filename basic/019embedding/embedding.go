845669b68ab0061128a7ffec8a207bb97140d777
Administrator <admin@example.com>
2022-01-17 11:25:45 +0800

    embedding

--- basic/019embedding/embedding.go
+++ basic/019embedding/embedding.go
@@ -1,1 +1,36 @@
-package _19embedding
+package main
+
+import "fmt"
+
+type base struct {
+	num int
+}
+
+func (b base) describe() string {
+	return fmt.Sprintf("base has num: %v", b.num)
+}
+
+type container struct {
+	//An embedding looks like a field without a name.
+	base
+	str string
+}
+
+func main() {
+	c := container{
+		base: base{num: 23},
+		str:  "jack",
+	}
+	fmt.Printf("c={num: %v, str: %v}\n", c.num, c.str)
+	fmt.Println("also num:", c.base.num)
+
+	//methods of base become methods of a container
+	fmt.Println(c.describe())
+
+	//container now implements the describer interface because it embeds base.
+	type describer interface {
+		describe() string
+	}
+	var d describer = c
+	fmt.Println(d.describe())
+}
