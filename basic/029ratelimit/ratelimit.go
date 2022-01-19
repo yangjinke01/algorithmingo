f551fe7c755db41f7561865ca1e05aac653c2709
Administrator <admin@example.com>
2022-01-19 14:45:45 +0800

    rate limit

--- basic/029ratelimit/ratelimit.go
+++ basic/029ratelimit/ratelimit.go
@@ -17,4 +17,25 @@ func main() {
 		<-limiter
 		fmt.Println("request:", request, time.Now())
 	}
+
+	burstLimiter := make(chan time.Time, 3)
+	for i := 0; i < 3; i++ {
+		burstLimiter <- time.Now()
+	}
+	go func() {
+		for tick := range time.Tick(200 * time.Millisecond) {
+			burstLimiter <- tick
+		}
+	}()
+
+	burstRequests := make(chan int, 5)
+	for i := 0; i < 5; i++ {
+		burstRequests <- i
+	}
+	close(burstRequests)
+
+	for request := range burstRequests {
+		<-burstLimiter
+		fmt.Println("request:", request, time.Now())
+	}
 }
